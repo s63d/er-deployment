5dc2b7721b81d78a28be0a48a1be7934fa7f4289
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-12 15:01:45 +0200

    removed swagger-combine

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 git submodule update
 git submodule foreach git pull origin master
 docker-compose down
-git submodule foreach ./gradlew build docker dockerTag
+git submodule foreach './gradlew build docker dockerTag || :' 
 docker-compose up -d
